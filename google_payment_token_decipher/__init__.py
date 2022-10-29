03811e9d90704801a273118c047e6064adc0b89e
Bastien Bouchoux <77078751+Bilibulle@users.noreply.github.com>
2022-10-29 20:36:05 +0200

    Update public_key_manager.py

---
+++ google_payment_token_decipher/__init__.py
@@ -0,0 +1,6 @@
+"""
+Allow simpler imports for public libraries classes and functions
+"""
+
+from google_payment_token_decipher.public_key_manager import PublicKeyManager
+from google_payment_token_decipher.public_key_manager import key_manager
--- google_payment_token_decipher/public_key_manager.py
+++ google_payment_token_decipher/public_key_manager.py
@@ -4,16 +4,20 @@ Public key manager
 from functools import lru_cache
 
 import requests
-from requests import RequestException
 
 from google_payment_token_decipher.exceptions import GetEcv2KeySetError
 from google_payment_token_decipher.exceptions import GetPublicKeySetsError
 from google_payment_token_decipher.models import Ecv2
 
+__all__ = [
+    "key_manager",
+    "PublicKeyManager",
+]
+
 
 class PublicKeyManager:
     """
-    container for
+    Public key manager
     """
 
     __KEYS_URL_PRODUCTION: str = (
@@ -26,17 +30,17 @@ class PublicKeyManager:
     def __init__(self, production: bool = False, refresh_background: bool = False):
         self.__production = production
         self.__refresh_background = refresh_background
-        self.data = self.get_keyset()
+        self.ecv2 = self.__retrive_keyset()
 
-    def refresh_keyset(self):
+    def refresh_keyset(self) -> None:
         """
-        Force lru reload
+        Force refresh the keyset
         """
-        ...
+        self.ecv2 = self.__retrive_keyset()
 
-    def get_keyset(self) -> Ecv2:
+    def __retrive_keyset(self) -> Ecv2:
         """
-        get google payment method token key-set converted to Ecv2 object
+        Get google payment method token key-set converted to Ecv2 object
         """
         try:
             response = requests.get(
@@ -53,11 +57,25 @@ class PublicKeyManager:
                     ),
                 )
             )
-        except RequestException as err:
+        except requests.RequestException as err:
             raise GetPublicKeySetsError from err
         except StopIteration as err:
             raise GetEcv2KeySetError from err
 
-    @lru_cache
-    def get_public_key(self):
-        return self.refresh_keyset()
+    def keyset(self) -> Ecv2:
+        """
+        Return the loaded key-set or refresh and return it if refresh background is set
+        """
+        if self.__refresh_background:
+            self.__retrive_keyset()
+        return self.ecv2
+
+
+@lru_cache()
+def key_manager(
+    production: bool = False, refresh_background: bool = False
+) -> PublicKeyManager:
+    """
+    Get a cached instance of the public key manager
+    """
+    return PublicKeyManager(production, refresh_background)
